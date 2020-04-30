637443149c384915b0dc74181dc6bd68123d34dc
breakfasting <breakfast.ing@gmail.com>
2020-04-30 13:25:04 -0700

    accepts_nested_attributes_for to create designs

--- app/controllers/api/designs_controller.rb
+++ app/controllers/api/designs_controller.rb
@@ -1,5 +1,5 @@
 class Api::DesignsController < ApplicationController
-  before_action :require_logged_in, only: [:show]
+  before_action :require_logged_in, only: [:show, :create]
 
   # def index
   #   if params[:query] == 'current'
@@ -16,11 +16,53 @@ class Api::DesignsController < ApplicationController
   end
 
   def create
+    @design = Design.new(create_design_params)
+    @design.user_id = current_user.id
+
+    if @design.save
+      render :show
+    else
+      render json: @design.errors.full_messages, status: 422
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def create_design_params
+    params.require(:design).permit(
+        :creator_id,
+        :title,
+        :description,
+        :public,
+        :width,
+        :height,
+        elements_attributes: [
+          :id,
+          :elementable_id,
+          :elementable_type,
+          :pos_x,
+          :pos_y,
+          :z_index,
+          :transparency,
+          :_destroy,
+          elementable_attributes: [
+            :id,
+            :width,
+            :height,
+            :color,
+            :shape,
+            :font_family,
+            :font_size,
+            :font_weight,
+            :text,
+            :_destroy
+          ]
+        ],
+      )
   end
 end

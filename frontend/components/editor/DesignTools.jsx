588bc0eb9bab21d9b1a2cfa9eef64a44b316e0cf
breakfasting <breakfast.ing@gmail.com>
2020-05-04 13:53:39 -0700

    update fields of elements

--- frontend/components/editor/DesignTools.jsx
+++ frontend/components/editor/DesignTools.jsx
@@ -5,6 +5,7 @@ class DesignTools extends React.Component {
   constructor(props) {
     super(props);
     this.state = { dropdown: null, selected: {}, selectedId: null };
+    this.updateStuff = this.updateStuff.bind(this);
   }
 
   componentDidUpdate(prevProps) {
@@ -32,21 +33,39 @@ class DesignTools extends React.Component {
     };
   }
 
-  render() {
+  updateStuff(e) {
+    e.preventDefault();
     const { selected, selectedId } = this.state;
     const { updateElement } = this.props;
+    updateElement(selectedId, selected);
+  }
+
+  render() {
+    const { selected } = this.state;
     if (Object.keys(selected).length === 0) {
       return (
         <div className={styles.designTools}>
-          Nothing selected
+          <span className={styles.nothingSelected}>Nothing selected</span>
         </div>
       );
     }
     return (
       <div className={styles.designTools}>
-        <button type="button" className="btn-color"> </button>
-        <form onSubmit={() => updateElement(selectedId, selected)}>
-          <input type="text" value={selected.elementableAttributes.color} onChange={this.changeValue('color')} />
+        <form
+          className={styles.designForm}
+          onSubmit={this.updateStuff}
+        >
+          <button type="button" className="btn-color" style={{ backgroundColor: selected.elementableAttributes.color }}> </button>
+          <input type="text" className="input-attr" size={selected.elementableAttributes.color.length} value={selected.elementableAttributes.color} onChange={this.changeValue('color')} />
+          <span>Width:</span>
+          <input type="text" className="input-attr" size={selected.elementableAttributes.width.toString().length} value={selected.elementableAttributes.width} onChange={this.changeValue('width')} />
+          <span>Height:</span>
+          <input type="text" className="input-attr" size={selected.elementableAttributes.height.toString().length} value={selected.elementableAttributes.height} onChange={this.changeValue('height')} />
+          <span>X:</span>
+          <input type="text" className="input-attr" size={selected.posX.toString().length} value={selected.posX} onChange={this.changeValue('posX')} />
+          <span>Y:</span>
+          <input type="text" className="input-attr" size={selected.posY.toString().length} value={selected.posY} onChange={this.changeValue('posY')} />
+          <button type="submit" className="btn-color">Submit</button>
         </form>
       </div>
     );
--- frontend/components/editor/elements/Shape.jsx
+++ frontend/components/editor/elements/Shape.jsx
@@ -5,7 +5,10 @@ const shapeGen = (shape, width, height, color) => {
     case 'rectangle':
       return <rect width={width} height={height} fill={color} />;
     case 'circle':
-      return <circle cx={width / 2} cy={height / 2} r={width / 2} fill={color} />;
+      if (width === height) {
+        return <circle cx={width / 2} cy={height / 2} r={width / 2} fill={color} />;
+      }
+      return <ellipse cx={width / 2} cy={height / 2} rx={width / 2} ry={height / 2} fill={color} />;
     default:
       return null;
   }

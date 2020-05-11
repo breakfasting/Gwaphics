4f2420d219de9269158a24331b6c955a2f94ec8c
breakfasting <breakfast.ing@gmail.com>
2020-05-10 17:06:00 -0700

    using deployed puppeteer server to take screenshot

--- frontend/util/thumb_util.js
+++ frontend/util/thumb_util.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 export const createThumbnail = ({ id, width, height }) => (
-  fetch(`http://localhost:4000/screenshot?id=${id}&width=${width}&height=${height}`)
+  fetch(`https://gwaphics-pup.herokuapp.com/screenshot?id=${id}&width=${width}&height=${height}`)
     .then((res) => res.blob())
     .then((blob) => {
       const file = new File([blob], 'File name', { type: 'image/png' });

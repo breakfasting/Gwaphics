573cb1d853dde0dbb2d5a43e4c9d2bdad7b35d72
breakfasting <breakfast.ing@gmail.com>
2020-06-29 20:08:57 -0700

    upload loading spin for uploadsDrawer

--- frontend/components/editor/drawers/UploadsDrawer.jsx
+++ frontend/components/editor/drawers/UploadsDrawer.jsx
@@ -12,6 +12,7 @@ class UploadsDrawer extends React.Component {
       width: 0,
       height: 0,
     };
+    this.handleFile = this.handleFile.bind(this);
   }
 
   componentDidMount() {
@@ -19,16 +20,85 @@ class UploadsDrawer extends React.Component {
     fetchUserUploads();
   }
 
+  handleFile(e) {
+    const reader = new FileReader();
+    const file = e.currentTarget.files[0];
+    reader.onloadend = () => {
+      const image = new Image();
+      image.onload = () => {
+        this.setState({
+          imageUrl: reader.result,
+          uploadedFile: file,
+          title: file.name,
+          width: image.width,
+          height: image.height,
+        }, this.uploadImage);
+      };
+      image.src = reader.result;
+    };
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      this.setState({ imageUrl: '', uploadedFile: null });
+    }
+  }
+
+  uploadImage() {
+    const {
+      title, uploadedFile, width, height,
+    } = this.state;
+    const { receiveUpload } = this.props;
+
+    const formData = new FormData();
+    formData.append('uploaded_image[title]', title);
+    if (uploadedFile) {
+      formData.append('uploaded_image[image]', uploadedFile);
+      formData.append('uploaded_image[width]', width);
+      formData.append('uploaded_image[height]', height);
+    }
+    $.ajax({
+      url: '/api/uploaded_images',
+      method: 'POST',
+      data: formData,
+      contentType: false,
+      processData: false,
+    }).then((payload) => {
+      this.setState({ imageUrl: '', uploadedFile: null });
+      receiveUpload(payload);
+    });
+  }
+
   render() {
     const { images } = this.props;
+    const { width, height, imageUrl } = this.state;
     return (
       <>
         <div className={styles.header}>
-          <button type="button" className="btn-blue">Upload an Image</button>
+          <label htmlFor="upload-button" className={styles.uploadBtn}>
+            Upload an Image
+            <input id="upload-button" className={styles.hidden} type="file" accept="image/*" onChange={this.handleFile} />
+          </label>
+          {/* <button type="button" className="btn-blue">Upload an Image</button> */}
         </div>
         <div className={scrollbar.customScroll}>
           <div className={styles.uploadsDrawer}>
             <div className={styles.masonry}>
+              {imageUrl && (
+                <div
+                  className={styles.masonItem}
+                  style={{
+                    flexGrow: (width * 85) / height,
+                    flexBasis: (width * 95) / height,
+                  }}
+                >
+                  <i style={{ paddingBottom: `${(height / width) * 100.0}%` }} />
+                  <img className={styles.tempImg} src={imageUrl} alt="" />
+                  <div className={styles.temp}>
+                    <div className={styles.loader} />
+                  </div>
+                </div>
+              )}
               {images.map((image) => (
                 <div
                   key={image.id}

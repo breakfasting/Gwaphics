03974c8fd0ce9a89ded95f132d9b7d4e77ccfafe
breakfasting <breakfast.ing@gmail.com>
2020-07-01 12:46:26 -0700

    API utils for unsplash

---
+++ frontend/util/unsplash_api_util.js
@@ -0,0 +1,29 @@
+export const fetchUnsplashQuery = (page, query) => (
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.unsplash.com/search/photos',
+    data: {
+      query,
+      per_page: 20,
+      page,
+    },
+    headers: {
+      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+    },
+  })
+);
+
+export const fetchUnsplashPopular = (page) => (
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.unsplash.com/photos',
+    data: {
+      order_by: 'popular',
+      per_page: 20,
+      page,
+    },
+    headers: {
+      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+    },
+  })
+);

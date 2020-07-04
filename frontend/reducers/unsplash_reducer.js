064c1049a7e32b5cb68ae9a36fe89ab7bc2cc135
breakfasting <breakfast.ing@gmail.com>
2020-07-03 21:36:24 -0700

    init cache for dev purposes

--- frontend/reducers/unsplash_reducer.js
+++ frontend/reducers/unsplash_reducer.js
@@ -3,7 +3,15 @@ import { RECEIVE_UNSPLASH_IMAGES } from '../actions/unsplash_actions';
 const unsplashReducer = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_UNSPLASH_IMAGES:
-      return action.images;
+      return action.images.map((image) => ({
+        thumb: image.urls.thumb,
+        full: image.urls.full,
+        download: image.links.download_location,
+        width: image.width,
+        height: image.height,
+        creator: image.user.name,
+        creator_link: image.user.links.html,
+      }));
     default:
       return state;
   }
--- frontend/util/unsplash_api_util.js
+++ frontend/util/unsplash_api_util.js
@@ -1,3 +1,5 @@
+import cache from './unsplash_cache';
+
 export const fetchUnsplashQuery = (page, query) => (
   $.ajax({
     method: 'GET',
@@ -13,17 +15,24 @@ export const fetchUnsplashQuery = (page, query) => (
   })
 );
 
-export const fetchUnsplashPopular = (page) => (
-  $.ajax({
-    method: 'GET',
-    url: 'https://api.unsplash.com/photos',
-    data: {
-      order_by: 'popular',
-      per_page: 20,
-      page,
-    },
-    headers: {
-      Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
-    },
-  })
-);
+// export const fetchUnsplashPopular = (page) => (
+//   $.ajax({
+//     method: 'GET',
+//     url: 'https://api.unsplash.com/photos',
+//     data: {
+//       order_by: 'popular',
+//       per_page: 20,
+//       page,
+//     },
+//     headers: {
+//       Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`,
+//     },
+//   })
+// );
+
+export const fetchUnsplashPopular = (page) => {
+  const promise = new Promise((resolve, reject) => {
+    resolve(cache);
+  });
+  return promise;
+};

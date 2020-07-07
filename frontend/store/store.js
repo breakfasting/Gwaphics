74fdd0590d5a3f6342c47dfacb6fce8a820e0c8b
breakfasting <breakfast.ing@gmail.com>
2020-07-07 12:46:47 -0700

    disable logger/navBar, redirect after login

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,7 +7,8 @@ const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger),
+    // applyMiddleware(thunk, logger),
+    applyMiddleware(thunk),
   )
 );
 
